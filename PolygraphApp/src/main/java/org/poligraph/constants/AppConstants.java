dd59bae36b83901e6cbf9281d0027411f8e8a2df
oakslist <oakslist@gmail.com>
2014-08-22 18:06:12 +0300

    second commit

---
+++ PolygraphApp/src/main/java/org/poligraph/constants/AppConstants.java
@@ -0,0 +1,8 @@
+package org.poligraph.constants;
+
+// see also http://franz.com/agraph/support/documentation/current/java-tutorial/jena-tutorial-40.html#A SPARQL Query
+// http://franz.com/agraph/support/learning/
+
+public class AppConstants {
+
+}
